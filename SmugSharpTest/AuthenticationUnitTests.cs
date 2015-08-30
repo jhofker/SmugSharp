5b168d8f7e21f66b93fc175d4c9ef8f236d6dacd
Jordan Hofker <jhofker@gmail.com>
2015-08-30 18:25:26 -0500

    Rename and move some things around.

---
+++ SmugSharpTest/AuthenticationUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using SmugSharp;
+
+namespace SmugSharpTest
+{
+    [TestClass]
+    public class AuthenticationUnitTests
+    {
+        private string ApiKey { get { return Configuration.Authentication.ApiKey; } }
+        private string ApiSecret { get { return Configuration.Authentication.ApiSecret; } }
+        private string CallbackUrl { get { return Configuration.Authentication.CallbackUrl; } }
+        private string AccessToken { get { return Configuration.Authentication.AccessToken; } }
+        private string AccessTokenSecret { get { return Configuration.Authentication.AccessTokenSecret; } }
+                              
+
+        [TestMethod]
+        public async Task TestGetRequestTokenForApiKey()
+        {
+            var smugmug = new SmugMug(ApiKey, ApiSecret, CallbackUrl);
+
+            var requestToken = await smugmug.Authentication.GetRequestToken();
+
+            Assert.IsNotNull(requestToken, "Request token should be not null.");
+
+            var cachedRT = await smugmug.Authentication.GetRequestToken();
+
+            Assert.AreEqual(requestToken, cachedRT);
+        }
+
+        [TestMethod]
+        public async Task TestAuthorizationUrl()
+        {
+            var smugmug = new SmugMug(ApiKey, ApiSecret, CallbackUrl);
+            var requestToken = await smugmug.Authentication.GetRequestToken();
+
+            var authUrl = smugmug.Authentication.GetAuthorizationUrl();
+
+            Assert.IsNotNull(authUrl);
+
+            Assert.IsTrue(authUrl.StartsWith("https"), "Authorization url must be secure.");
+        }
+    }
+}
---
+++ SmugSharpTest/SmugMugUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using SmugSharp;
+
+namespace SmugSharpTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string ApiKey { get { return Configuration.Authentication.ApiKey; } }
+        private string ApiSecret { get { return Configuration.Authentication.ApiSecret; } }
+        private string CallbackUrl { get { return Configuration.Authentication.CallbackUrl; } }
+        private string AccessToken { get { return Configuration.Authentication.AccessToken; } }
+        private string AccessTokenSecret { get { return Configuration.Authentication.AccessTokenSecret; } }
+
+
+        [TestMethod]
+        public void TestSmugMugCtorWorks()
+        {
+            var smugmug = new SmugMug(ApiKey, ApiSecret, CallbackUrl);
+
+            Assert.IsNotNull(smugmug, "SmugMug should not be null after constructing.");
+            Assert.IsNotNull(smugmug.Authentication, "SmugMug Authentication should not be null.");
+            Assert.AreEqual(ApiKey, smugmug.ApiKey, "API key should be kept.");
+        }
+
+        [TestMethod]
+        public async Task TestAuthorization()
+        {
+            var smugmug = new SmugMug(AccessToken, AccessTokenSecret, ApiKey, ApiSecret, CallbackUrl);
+
+            var authUserUrl = $"{SmugMug.BaseApiUrl}!authuser";
+            var response = await smugmug.GetResponseWithHeaders(authUserUrl);
+
+            Logger.LogMessage(response);
+            Assert.IsFalse(response.Contains("\"Code\":4"));
+        }
+    }
+}
