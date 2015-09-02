34ecfbc28b9cfcb011c185ae8c296219c1e4488f
Jordan Hofker <jhofker@gmail.com>
2015-09-02 00:27:49 -0500

    Tests for Node class

---
+++ SmugSharpTest/NodeUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using SmugSharp;
+using SmugSharpTest.Configuration;
+
+namespace SmugSharpTest
+{
+    [TestClass]
+    public class NodeUnitTests : UnitTestsBase
+    {
+        [TestMethod]
+        public async Task TestRootNodeHasChildren()
+        {
+            var smugmug = new SmugMug(AccessToken, AccessTokenSecret, ApiKey, ApiSecret, CallbackUrl);
+
+            var user = await smugmug.GetCurrentUser();
+            var node = await user.GetRootNode();
+
+            Assert.IsTrue(node.HasChildren);
+
+            Assert.IsTrue(node.Children.Count > 0);
+        }
+    }
+}
