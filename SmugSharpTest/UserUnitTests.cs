4cbc2c85a577e93fcd48cdbcb65d71adedafc608
Jordan Hofker <jhofker@gmail.com>
2015-09-02 00:27:44 -0500

    Tests for User class
    Yes I know these are integration tests...

---
+++ SmugSharpTest/UserUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using SmugSharp;
+using SmugSharpTest.Configuration;
+
+namespace SmugSharpTest
+{
+    [TestClass]
+    public class UserUnitTests : UnitTestsBase
+    {               
+        [TestMethod]
+        public async Task TestGetCurrentUserBioImageNotNull()
+        {
+            var smugmug = new SmugMug(AccessToken, AccessTokenSecret, ApiKey, ApiSecret, CallbackUrl);
+
+            var user = await smugmug.GetCurrentUser();
+            var bioImage = await user.GetBioImage();
+
+            Assert.IsNotNull(bioImage);
+        }
+
+        [TestMethod]
+        public async Task TestGetCurrentUserRootNodeNotNull()
+        {
+            var smugmug = new SmugMug(AccessToken, AccessTokenSecret, ApiKey, ApiSecret, CallbackUrl);
+
+            var user = await smugmug.GetCurrentUser();
+            var node = await user.GetRootNode();
+
+            Assert.IsNotNull(node);
+        }
+    }
+}
