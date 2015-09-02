cf340e57bfe79be92a4bb6b512eb999ee13052c6
Jordan Hofker <jhofker@gmail.com>
2015-09-02 00:27:04 -0500

    Base class for unit tests to hold auth info

---
+++ SmugSharpTest/Configuration/UnitTestsBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmugSharpTest.Configuration
+{
+    public class UnitTestsBase
+    {
+        protected string ApiKey { get { return Configuration.Authentication.ApiKey; } }
+        protected string ApiSecret { get { return Configuration.Authentication.ApiSecret; } }
+        protected string CallbackUrl { get { return Configuration.Authentication.CallbackUrl; } }
+        protected string AccessToken { get { return Configuration.Authentication.AccessToken; } }
+        protected string AccessTokenSecret { get { return Configuration.Authentication.AccessTokenSecret; } }
+    }
+}
