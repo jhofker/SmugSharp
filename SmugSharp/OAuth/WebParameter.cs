257f5f5999b3272f9ff5bf8fde8de0c59496a007
Jordan Hofker <jhofker@gmail.com>
2016-04-04 01:32:05 -0500

    Add OAuth from https://github.com/danielcrenna/oauth
    Modified to work with UWP

---
+++ SmugSharp/OAuth/WebParameter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OAuth
+{
+    [DebuggerDisplay("{Name}:{Value}")]
+    public class WebParameter
+    {
+        public WebParameter(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Value { get; set; }
+        public string Name { get; private set; }
+    }
+}
